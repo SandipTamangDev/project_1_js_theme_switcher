f6688952449a7721f6a77f2f8f905a7ab766f7e6
Sandip Tamang <136832190+SandipTamangDev@users.noreply.github.com>
2025-11-22 13:49:33 +0545

    logic updated

--- js/script.js
+++ js/script.js
@@ -2,11 +2,34 @@ const button = document.getElementById('button');
 let theme;
 const savedTheme = localStorage.getItem("theme");
 
+function getSystemTheme() {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        return "Dark";
+    } else {
+        return "Light";
+    }   
+}
+
 if (savedTheme) {
     button.innerText = savedTheme;
 
     if (savedTheme === "Dark") {
-        document.documentElement.classList.add("dark");
+        document.documentElement.classList.add("Dark");
+    }
+
+    if (savedTheme === "Light") {
+        document.documentElement.classList.add("Light");
+    }
+} else {
+    const systemTheme = getSystemTheme();
+    button.innerText = "systemTheme";
+
+    if (systemTheme === "Dark") {
+        document.documentElement.classList.add("Dark")
+    } 
+
+    if (systemTheme === "Light") {
+        document.documentElement.classList.add("Light");
     }
 }
 
@@ -16,12 +39,15 @@ button.addEventListener('click', () => {
     if (button.innerText === "Light") {
         button.innerText = "Dark"
         theme = "Dark"
+        document.documentElement.classList.remove("Light");
+        document.documentElement.classList.add("Dark");
+
     } else {
         button.innerText = "Light";
         theme = "Light"
+        document.documentElement.classList.add("Light");
+        document.documentElement.classList.remove("Dark");
     }
-
-    document.documentElement.classList.toggle("dark");
 
     localStorage.setItem("theme", theme);
 });
