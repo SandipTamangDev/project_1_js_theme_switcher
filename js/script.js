91c386e7ac3c84a644476f70c9ff5dacdab7a231
Sandip Tamang <136832190+SandipTamangDev@users.noreply.github.com>
2025-11-22 11:05:36 +0545

    saves theme and renders that theme again.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,30 @@
 const button = document.getElementById('button');
+let theme;
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme) {
+    button.innerText = savedTheme;
+
+    if (savedTheme === "Dark") {
+        document.documentElement.classList.add("dark");
+    }
+}
 
 button.addEventListener('click', () => {
 
 
-    if ( button.innerText === "Light") {
+    if (button.innerText === "Light") {
         button.innerText = "Dark"
+        theme = "Dark"
     } else {
         button.innerText = "Light";
+        theme = "Light"
     }
+
     document.documentElement.classList.toggle("dark");
+
+    localStorage.setItem("theme", theme);
 });
+
+
 
